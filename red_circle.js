1ef7b591d66f0cea2507c5eee17b06ec0837641b
jovanohks <jovanoh400@gmail.com>
2025-07-12 09:16:04 +0000

    Jul 12, 2025, 9:16 AM

--- red_circle.js
+++ red_circle.js
@@ -7,5 +7,5 @@ function draw(){
     fill(255, 0, 0); //rgb
     ellipse(300,100,100,100); // x-axis , y-axis, size
     fill(0, 255, 0); //rgb
-    ellipse(300,100,100,100); 
+    ellipse(300,200,100,100); 
 }
