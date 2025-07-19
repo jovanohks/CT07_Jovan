d5a1e1e9d1869c8523bc343b75a523798fb828d7
jovanohks <jovanoh400@gmail.com>
2025-07-19 09:01:23 +0000

    Jul 19, 2025, 9:01 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -32,11 +32,13 @@ function setup() {
 
 function draw() {
   // write your codes here
+  background(240)
   if (ball.x <0 + ball.diameter /2 || ball.x > width - ball.diameter /2){
     ball.vel.x *=-1
   }
   if (ball.y <0 + ball.diameter /2 || ball.y > height - ball.diameter /2){
     ball.vel.y *=-1
   }
-  box.x=MouseEvent.x
+  box.x=mouseX;
+  box.y=mouseY;
 }
