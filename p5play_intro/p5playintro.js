88040796899fcc9ba774fc03ab9636c5f11d4715
jovanohks <jovanoh400@gmail.com>
2025-07-19 08:39:37 +0000

    Jul 19, 2025, 8:39 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -7,7 +7,7 @@ function setup() {
   // write your codes here
   fill("skyblue");
   stroke("pink");
-  strokeWeight = 10;
+  strokeWeight = 2;
   circle(30,30,50);
   rect(50,50,100,200);
   // End Basic shape testing
