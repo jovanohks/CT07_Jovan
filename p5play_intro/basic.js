fbb346f16590625d5b2a8f2b8184187d89cba3fc
jovanohks <jovanoh400@gmail.com>
2025-07-19 09:23:43 +0000

    Jul 19, 2025, 9:23 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -22,6 +22,8 @@ function setup() {
 function draw(){
   eclipse(100,100,80,50);
   circle(200,200,30);
-  rect(30,30,55,50,20,15,10,5);3
+  rect(30,30,55,50,20,15,10,5);
+  nofill();
+  
   triangle(30,75,58,20,86,75);
 }
