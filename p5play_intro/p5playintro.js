fe5320111c157b0b6dab04cecddc5e2cd2c8fd87
jovanohks <jovanoh400@gmail.com>
2025-07-19 09:02:45 +0000

    Jul 19, 2025, 9:02 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -35,7 +35,7 @@ function draw() {
   background(240);
     fill(0);
     textSize(16);
-    text("Ball:"+)
+    text("Ball:"+int(ball.x)+",")
   if (ball.x <0 + ball.diameter /2 || ball.x > width - ball.diameter /2){
     ball.vel.x *=-1
   }
