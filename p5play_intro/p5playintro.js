7f45676c7a74cc24731a20b79cb4290739918bb4
jovanohks <jovanoh400@gmail.com>
2025-07-19 08:26:11 +0000

    Jul 19, 2025, 8:26 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -2,7 +2,7 @@ function setup() {
   // Set up the canvas
   new Canvas(800, 400);
   background(250); //background color
-
+  ball = new Sprite();
   // Basic shape testing
   // write your codes here
  
