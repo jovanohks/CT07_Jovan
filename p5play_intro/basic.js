a0e5d7edda642b90df0fa367b5bde3da7b4f02df
jovanohks <jovanoh400@gmail.com>
2025-07-26 08:29:16 +0000

    Jul 26, 2025, 8:29 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -17,6 +17,9 @@ function setup() {
   for(let i =1; i < 10; i++){
     console.log("counting... "+i)
   }
+  for(let i=20; i>1;i++){
+    
+  }
   // --- Exercise: Age category classification ---
   // write your codes here
 
