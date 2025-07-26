df28b9ee218e968220d05bd370918cb3854064c6
jovanohks <jovanoh400@gmail.com>
2025-07-26 08:08:45 +0000

    Jul 26, 2025, 8:08 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -1,7 +1,7 @@
 function setup() {
   // write your codes here
   createCanvas(800,600)
-  //background(220);-->
+  background(220);-->
   console.log("hello")
   // --- Exercise: Area of Triangle ---
   // write your codes here
