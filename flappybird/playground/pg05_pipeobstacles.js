4aa8ba55180a3700fa6bf43bca39d3cc7e0cf199
jovanohks <jovanoh400@gmail.com>
2025-08-16 08:29:21 +0000

    Aug 16, 2025, 8:29 AM

--- flappybird/playground/pg05_pipeobstacles.js
+++ flappybird/playground/pg05_pipeobstacles.js
@@ -53,5 +53,6 @@ function draw(){
     text('vel.y: '+bird.vel.y.toFixed(2),10,20);
     text('isMoving' + bird.isMoving,10,40);
     text('isSleeping'+bird.isSleeping,10,60);
+    if(frameCount===1)
 }
 
