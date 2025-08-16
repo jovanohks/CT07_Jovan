398cefd37f7e52876fc4251553bc91747c40621e
jovanohks <jovanoh400@gmail.com>
2025-08-16 08:25:52 +0000

    Aug 16, 2025, 8:25 AM

--- flappybird/playground/pg05_pipeobstacles.js
+++ flappybird/playground/pg05_pipeobstacles.js
@@ -1,1 +1,54 @@
-_
+// write your codes here
+let bird,floor;
+let flapMidImg,bg,base;
+function preload(){
+    flapMidImg = loadImage('assets/yellowbird-midflap.png');
+    bg=loadImage('assets/background-night.png');
+    base=loadImage('assets/base.png');
+    flapUpImg= loadImage('assets/yellowbird-upflap.png');
+    flapDownImg= loadImage('assets/yellowbird-downflap.png');
+}
+function setup(){
+    new Canvas(400,600);
+    bird = new Sprite();
+    bird.x = width /2;
+    bird.y=200;
+    bird.width = 30;
+    bird.height=30;
+    bird.img = flapMidImg;
+    bird.collider="dynamic"
+    bird.mass=2;
+    bird.drag=0.05;
+    bird.bounciness=10;
+    world.gravity.y=10;
+    floor= new Sprite();
+    floor.x = 200;
+    floor.y = height -20;
+    floor.width = 400;
+    floor.height= 125;
+    floor.collider = "static";
+    floor.img = base;
+}
+function draw(){
+    image(bg,0,0,width,height);
+    if(kb.presses('space') || mouse.presses('left')){
+        bird.vel.y = -5;
+        bird.sleeping = false;
+    }
+    if(bird.vel.y <-1){
+        bird.img = flapUpImg;
+        bird.rotation=-30;
+    }else if (bird.vel.y >1){
+        bird.img = flapDownImg;
+        bird.rotation=30;
+    }else{
+        bird.img=flapMidImg;
+        bird.rotation = 0;
+    }
+    fill("blue");
+    textSize(14);
+    text('vel.y: '+bird.vel.y.toFixed(2),10,20);
+    text('isMoving' + bird.isMoving,10,40);
+    text('isSleeping'+bird.isSleeping,10,60);
+}
+
