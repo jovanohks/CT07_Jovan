a3d864f4b3222fa3552690d74a1b98ebdc240248
jovanohks <jovanoh400@gmail.com>
2025-08-23 08:30:50 +0000

    Aug 23, 2025, 8:30 AM

--- flappybird/playground/revision.js
+++ flappybird/playground/revision.js
@@ -7,9 +7,6 @@ function setup(){
     new Canvas(400,600);
     bird = new Sprite();
     bird.x = width/2;
-        new Canvas(400,600);
-    bird = new Sprite();
-    bird.x = width /2;
     bird.y=200;
     bird.width = 30;
     bird.height=30;
