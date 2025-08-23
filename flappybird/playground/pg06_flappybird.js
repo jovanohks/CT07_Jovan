66d008f2e7c1ca0e2534c56f0e97b30ba919632a
jovanohks <jovanoh400@gmail.com>
2025-08-23 09:36:00 +0000

    Aug 23, 2025, 9:36 AM

--- flappybird/playground/pg06_flappybird.js
+++ flappybird/playground/pg06_flappybird.js
@@ -71,11 +71,11 @@ function draw(){
     text('isMoving' + bird.isMoving,10,40);
     text('isSleeping'+bird.isSleeping,10,60);
     while (1==1){
-    if(frameCount %90===0){
-        
-        spawnPipePair();
+        if(frameCount %90===0){
+            
+            spawnPipePair();
+        }
     }
-}
 }
 function spawnPipePair(){
     let gap=50;
