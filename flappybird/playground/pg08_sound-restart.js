a9aac12fb1f9d5dbcedcfc3d86919111249657b3
jovanohks <jovanoh400@gmail.com>
2025-09-13 09:27:01 +0000

    Sep 13, 2025, 9:27 AM

--- flappybird/playground/pg08_sound-restart.js
+++ flappybird/playground/pg08_sound-restart.js
@@ -137,7 +137,7 @@ function draw(){
                 gameoverLabel.remove();
                 startScreenLabel.visible = true;
                 startScreenLabel.x = bird.x;
-                startScreenLabel.y = height;
+                startScreenLabel.y = height/2 -50;
                 loop();
             },3000);
         }
