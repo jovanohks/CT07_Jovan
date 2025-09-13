6cad3f8fee520630f8baa354b788234cc74a8d3a
jovanohks <jovanoh400@gmail.com>
2025-09-13 09:10:09 +0000

    Sep 13, 2025, 9:10 AM

--- flappybird/playground/pg08_sound-restart.js
+++ flappybird/playground/pg08_sound-restart.js
@@ -82,6 +82,7 @@ function draw(){
         if(bird.vel.y <-1){
             bird.img = flapUpImg;
             bird.rotation=-30;
+            
         }else if (bird.vel.y >1){
             bird.img = flapDownImg;
             bird.rotation=30;
