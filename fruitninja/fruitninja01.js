2090da6c66625e79a183707b995b56db68f26c7f
jovanohks <jovanoh400@gmail.com>
2025-09-27 09:45:53 +0000

    Sep 27, 2025, 9:45 AM

--- fruitninja/fruitninja01.js
+++ fruitninja/fruitninja01.js
@@ -64,7 +64,7 @@ function splitFruit(x,y,fruitData){
     right.vel.y = -3;
     right.rotationSpeed = 5;
     score +=1;
-
+    console.log(score);
 }
 function spawnFruit(){
     let fruitData = random(fruitTypes);
