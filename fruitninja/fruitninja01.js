005dcdf653e74dcdecb07f4408eb6fea60dd05a7
jovanohks <jovanoh400@gmail.com>
2025-09-27 09:13:23 +0000

    Sep 27, 2025, 9:13 AM

--- fruitninja/fruitninja01.js
+++ fruitninja/fruitninja01.js
@@ -38,7 +38,7 @@ function sliceFruit(){
         }
 
     
-    let d = dist(mouse,x,mouse.y,fruit.x,fruit.y);
+    let d = dist(mouse.x,mouse.y,fruit.x,fruit.y);
     if (d < (fruit.d /2)){
             fruit.sliced = true;
             const fx = fruit.x;
