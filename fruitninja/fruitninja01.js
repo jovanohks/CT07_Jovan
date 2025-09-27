aa398a2d484d2d81d9f41bb9b203c561d7a3e781
jovanohks <jovanoh400@gmail.com>
2025-09-27 08:08:38 +0000

    Sep 27, 2025, 8:08 AM

--- fruitninja/fruitninja01.js
+++ fruitninja/fruitninja01.js
@@ -23,5 +23,6 @@ function draw(){
 function spawnFruit(){
     let fruitData = random(fruitTypes);
     let randomX = random(300,500);
-    let fruit = new fruitGroup.Sprite(randomX,height+20,40)
+    let fruit = new fruitGroup.Sprite(randomX,height+20,40);
+    fruit.image = fruit
 }
