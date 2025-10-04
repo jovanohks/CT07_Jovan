9d70ad9ab4027a18c1274969eebabbaa40ab74a7
jovanohks <jovanoh400@gmail.com>
2025-10-04 08:30:05 +0000

    Oct 4, 2025, 8:30 AM

--- fruitninja/fruitninja01.js
+++ fruitninja/fruitninja01.js
@@ -30,7 +30,8 @@ function draw(){
         textSize(40);
         text('Fruit Ninja',width /2 ,height /2 ,40);
         textSize(24);
-        text("Press SPACE or CLICK to start",width /2 ,);
+        text("Press SPACE or CLICK to start",width /2 ,height /2 ,20);
+        return;
     }
     if (frameCount % 60 == 0){
         spawnFruit();
