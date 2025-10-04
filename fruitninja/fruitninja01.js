8f9e43d27fec8f198a4488f6960e1b4ae88d0b0d
jovanohks <jovanoh400@gmail.com>
2025-10-04 08:54:19 +0000

    Oct 4, 2025, 8:54 AM

--- fruitninja/fruitninja01.js
+++ fruitninja/fruitninja01.js
@@ -11,8 +11,8 @@ function preload(){
     let peach = {whole: loadImage('assets/peachwhole.png'),half1: loadImage('assets/peachhalf.png')};
     let watermelon = {whole:loadImage("assets/watermelonwhole.png") ,half1: loadImage('assets/watermelonhalf.png')};
     let banana ={whole: loadImage('assets/bananawhole-removebg-preview (1).png'),half1: loadImage('assets/bhalf2-removebg-preview (1).png')}
-    fruitTypes = [peach,watermelon,banana];
-    
+    //fruitTypes = [peach,watermelon,banana];
+    fruitTypes = [peach,watermelon]
 }
 function setup(){
     new Canvas(800,600);
