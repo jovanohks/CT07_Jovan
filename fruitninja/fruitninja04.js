dfca2c59641c90df0acb507cb0bad6a5cd2c20a1
jovanohks <jovanoh400@gmail.com>
2025-10-11 08:30:03 +0000

    Oct 11, 2025, 8:30 AM

--- fruitninja/fruitninja04.js
+++ fruitninja/fruitninja04.js
@@ -15,7 +15,7 @@ function preload(){
     let watermelon = {whole:loadImage("assets/watermelonwhole.png") ,half1: loadImage('assets/watermelonhalf.png')};
     let banana ={whole: loadImage('assets/bananawhole-removebg-preview (1).png'),half1: loadImage('assets/bhalf2-removebg-preview (1).png')}
     //fruitTypes = [peach,watermelon,banana];
-    
+    let sliceSound = 
     fruitTypes = [peach,watermelon];
 }
 function setup(){
