9d4002f5b5a70912f62515dc964c3126691b3547
jovanohks <jovanoh400@gmail.com>
2025-10-18 08:17:52 +0000

    Oct 18, 2025, 8:17 AM

--- fruitninja/fruitninja05.js
+++ fruitninja/fruitninja05.js
@@ -78,6 +78,7 @@ function draw(){
     if (frameCount % 60 == 0){
         spawnFruit();
     }
+    
     if (mouse.pressing()){
         trail = new Sprite(mouse.x,mouse.y,7);
         trail.collider = 'none';
