d6e411115c63dabb54db2b6adcf337ce33414311
jovanohks <jovanoh400@gmail.com>
2025-10-18 08:43:36 +0000

    Oct 18, 2025, 8:43 AM

--- p5play_intro/basic2.js
+++ p5play_intro/basic2.js
@@ -1,9 +1,9 @@
 let inputText;
-let UserText = "your text here";
+let userText = "your text here";
 function setup(){
     createCanvas(600,400);
     inputText = createInput();
     inputText.position(200,height - 80);
-    inputText.input(pdateText);
+    inputText.input(updateText);
 
 }
