5e3e78c214a987451fec6759e2d7fe121d7e18c2
jovanohks <jovanoh400@gmail.com>
2025-10-25 08:56:50 +0000

    Oct 25, 2025, 8:56 AM

--- p5play_intro/basic2_1.js
+++ p5play_intro/basic2_1.js
@@ -1,18 +1,18 @@
-let tboxText = "input something here";
-function setup(){
-    
+    let tboxText = "input something here";
+    function setup(){
+
     createCanvas(600,400);
     tbox = createInput();
     tbox.position(80,80);
     tbox.input(updateText);
     colorPicker = createColorPicker("#ffffff");
     colorPicker.position(80,120);
-}
-function draw(){
+    }
+    function draw(){
     background(220);
     fill(colorPicker.value())
     text(tboxText,50,50);
-}
-function updateText(){
+    }
+    function updateText(){
     tboxText = this.value();
-}
+    }
