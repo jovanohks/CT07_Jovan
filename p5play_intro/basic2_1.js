40edeadc65087075ed23909d1be6988990ebcc5a
jovanohks <jovanoh400@gmail.com>
2025-10-25 08:45:05 +0000

    Oct 25, 2025, 8:45 AM

--- p5play_intro/basic2_1.js
+++ p5play_intro/basic2_1.js
@@ -2,12 +2,12 @@ function setup(){
     new Canvas(200,200)
     tbox = createInput();
     tbox.position(80,80);
-
+    tbox.input(updateText);
 
 }
 function draw(){
 
 }
 function updateText(){
-    
+
 }
