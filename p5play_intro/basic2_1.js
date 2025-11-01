fde02688227189b0db4e57b0dc5215e98f025ac8
jovanohks <jovanoh400@gmail.com>
2025-11-01 08:07:57 +0000

    Nov 1, 2025, 8:07 AM

--- p5play_intro/basic2_1.js
+++ p5play_intro/basic2_1.js
@@ -28,5 +28,5 @@ function updatenoun(){
     text(verbinput.value(),320,120);
     text(adjinput.value(),340,120);
     text(place.value(),360,120);
-    text(adverb.value())
+    text(adverb.value(),380,120)
 }
