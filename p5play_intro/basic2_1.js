601493a904c8790fa23ad5490feb23716276d2d4
jovanohks <jovanoh400@gmail.com>
2025-11-08 08:30:30 +0000

    Nov 8, 2025, 8:30 AM

--- p5play_intro/basic2_1.js
+++ p5play_intro/basic2_1.js
@@ -96,7 +96,7 @@ function draw(){
 function checkGuess(){
     text(inputbox.value(),150,50);
     let guess = inputbox.value().toLowerCase();
-    console.log(getCorrectLetters)
+    console.log(getCorrectLetters(guess,selectedWord));
 }
 function getCorrectLetters(guess,word){
     let correctletter ="";
