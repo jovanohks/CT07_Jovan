89cd60826e294f547bfa46a2620894dba6c3d031
jovanohks <jovanoh400@gmail.com>
2025-11-08 09:05:43 +0000

    Nov 8, 2025, 9:05 AM

--- p5play_intro/basic2_1.js
+++ p5play_intro/basic2_1.js
@@ -92,7 +92,7 @@ function draw(){
     //submitButton.mousePresed(checkGuess);
     //text(inputbox.value(),150,50);
     if (attempts <10){
-        text("Game Over! ")
+        text("Game Over! Correct word was "+ selectedWord,200,)
     }
 }
 function checkGuess(){
