ec2e2696e3f158065cfcf85b426297aab146aee9
jovanohks <jovanoh400@gmail.com>
2025-11-08 08:50:40 +0000

    Nov 8, 2025, 8:50 AM

--- p5play_intro/basic2_1.js
+++ p5play_intro/basic2_1.js
@@ -105,7 +105,7 @@ function checkGuess(){
         message = "too many letters"
     }else{
         let corectLetter = getCorrectLetters(guess,selectedWord);
-        
+        message = "Wrong"
     }
 }
 function getCorrectLetters(guess,word){
