10cb7b2bd3e12eb7d0a1fb0d273a270377f4e197
jovanohks <jovanoh400@gmail.com>
2025-11-08 08:45:18 +0000

    Nov 8, 2025, 8:45 AM

--- p5play_intro/basic2_1.js
+++ p5play_intro/basic2_1.js
@@ -94,8 +94,9 @@ function draw(){
 
 }
 function checkGuess(){
-    text(inputbox.value(),150,50);
+    
     let guess = inputbox.value().toLowerCase();
+    
     console.log(getCorrectLetters(guess,selectedWord));
 }
 function getCorrectLetters(guess,word){
